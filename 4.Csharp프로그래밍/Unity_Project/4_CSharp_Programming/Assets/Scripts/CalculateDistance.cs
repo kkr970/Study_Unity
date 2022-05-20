4ccfba27500d897e10722017e537f6e5945f9862
kkr970 <58022426+kkr970@users.noreply.github.com>
2022-05-20 15:53:18 +0900

    4.CSharp 프로그래밍

---
+++ 4.Csharp프로그래밍/Unity_Project/4_CSharp_Programming/Assets/Scripts/CalculateDistance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateDistance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2, 2)에서 (5, 6)까지의 거리 : " + distance);
+    }
+
+    float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float width = x2 - x1;
+        float height = y2 - y1;
+
+        float distance = width * width + height * height;
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
+    }
+}
+
+//이후의 for문, if문, while문, 배열 등 알고있는 내용은 스킵
---
+++ 4.Csharp프로그래밍/Unity_Project/4_CSharp_Programming/Assets/Scripts/HelloCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    void Start()
+    {
+        string characterName = "유니티";
+        char bloodType = 'A';
+        int age = 100;
+        //float는 소수점 7자리까지만 정확하게 표현, 8자리부터는 근삿값으로 처리되어짐
+        float height = 123.456f;
+        bool isAI = true;
+
+        Debug.Log("캐릭터 이름 : " + characterName);
+        Debug.Log("혈액형 : " + bloodType);
+        Debug.Log("나이 : " + age);
+        Debug.Log("키 : " + height);
+        Debug.Log("인공지능인가? : " + isAI);
+    }
+}
