434fabc7412cd0956bea606daa331421db48d4c5
kkr970 <58022426+kkr970@users.noreply.github.com>
2022-05-24 14:31:09 +0900

    7.닷지 탄알제작

---
+++ 6.탄막슈팅게임_닷지/Dodge/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8.0f;
+    private Rigidbody bulletRigidbody;
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3.0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
