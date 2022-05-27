8d4cf08f56f0d434eec4e3c5e6c703fedc974e33
kkr970 <58022426+kkr970@users.noreply.github.com>
2022-05-27 13:39:26 +0900

    10.공간과 움직임

---
+++ 10.공간과_움직임/Space/Assets/Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform childTransfrom;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, -1, 0);
+        childTransfrom.localPosition = new Vector3(0, 2, 0);
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
+        childTransfrom.localRotation = Quaternion.Euler(new Vector3(0, 60, 0));
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 100) * Time.deltaTime);
+            childTransfrom.Rotate(new Vector3(0, 100, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -100) * Time.deltaTime);
+            childTransfrom.Rotate(new Vector3(0, -100, 0) * Time.deltaTime);
+        }
+    }
+}
